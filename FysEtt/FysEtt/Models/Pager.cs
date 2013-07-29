39de608d5fd6eb039d16de4b841fe8c15fd72b17
tokerranta <rantanikunen.markus@gmail.com>
2013-07-29 22:12:52 +0200

    Created some viewmodels

---
+++ FysEtt/FysEtt/Models/Pager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FysEtt.Models
+{
+    public class Pager
+    {
+        public int CurrentPage { get; set; }
+        public int ItemsCount { get; set; }
+        public int PageSize { get; set; }
+        public int Pages { get; set; }
+
+        public Pager(int page, int itemsCount, int pageSize)
+        {
+            CurrentPage = page;
+            ItemsCount = itemsCount;
+            PageSize = pageSize;
+            Pages = (int)Math.Ceiling((double)itemsCount / pageSize);
+        }
+    }
+}
---
+++ FysEtt/FysEtt/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FysEtt.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required, Compare("Password", ErrorMessage = "Lösenorden matchar inte!")]
+        public string RepeatedPassword { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
